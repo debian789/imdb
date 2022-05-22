453afa86639545f7ffbd1b86e0f1b6d00f79f4fe
Miguel Suescun <debian789@gmail.com>
2022-05-22 16:38:51 -0500

    Limpieza de codigo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -36,11 +36,6 @@ export default function HomeScreen(props) {
     let index = indexInput;
     let position = positionInput;
     const paginate = [];
-    // console.log("disque cargo la data " + data.length);
-    // let index = indexItem;
-    // let position = positionItem;
-    // console.log(`indice: ${index} - position ${position}`);
-    //console.log(`dato validacion: ${data.length >= index}`);
     if (data.length >= index) {
       for (var i = 1; pagination >= i; i++) {
         if (data.length > position) {
@@ -48,39 +43,22 @@ export default function HomeScreen(props) {
         }
         position = index + i;
       }
-      //console.log("se ejecuta esto ??? ");
     }
-
-    console.log(`1) Informaciòn data ${data.length}`);
-    console.log(`2) Informaciòn index ${indexInput}`);
-    console.log(`3) Informaciòn positionInput ${positionInput}`);
-    console.log(`4) Informaciòn oldData ${oldData.length}`);
-    console.log(`5) Informacion paginate  ${paginate.length}`);
-    // setDataPaginate([...oldData, ...paginate]);
     setIndexItem(index + pagination);
     setPositionItem(position);
-
     setDataPaginate([...oldData, ...paginate]);
-    //return [...oldData, ...paginate];
   };
 
   const loadMovies = async () => {
     try {
-      //console.log(`2) indice: ${indexItem} - position ${positionItem}`);
       if (movieSearch && movieSearch.length > 3) {
         const dataServer = await searchMovie(movieSearch);
         resetSearch();
         saveSearchStorage(dataServer.results);
-        /*
-        console.log("estoy aqui");
-        console.log(dataServer.results.length);
-        console.log(dataPaginate.length);*/
-        // setDataPaginate(await loadPaginationData(0, 0, []));
         loadPaginationData(0, 0, []);
         setIsNewData(true);
       }
     } catch (error) {
-      // saveSearchStorage([]);
       console.error(error);
     }
   };
